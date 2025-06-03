d425e6cf4dc9fb8735ada717798cb9e07de89043
unknown <parth.gupt07@gmail.com>
2025-06-03 13:18:04 +0530

    removed the build errors

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,17 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+type ThemeProviderProps = {
+  children: React.ReactNode
+  attribute?: string
+  defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+  storageKey?: string
+  themes?: string[]
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
